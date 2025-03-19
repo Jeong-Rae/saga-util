969a7db73ecfd72149b82ac9c706e4c83d9c5ffa
JRJR519 <kkwjdfo@gmail.com>
2025-03-20 06:04:16 +0900

    feat(Context) : sync 지원 추가

--- src/LocalTransaction/LocalTransactionContext.ts
+++ src/LocalTransaction/LocalTransactionContext.ts
@@ -1,17 +1,25 @@
 import {RollbackFailedError} from "./RollbackFailedError";
 
+export type SyncRollbackFn<T = void> = () => T;
+export type AsyncRollbackFn<T = void> = () => Promise<T>;
 export type RollbackFn<T = void> = () => Promise<T>;
 
 export type RollbackablePromise<T> = Promise<T> & {
-    rollback: <U>(rollbackFn: RollbackFn<U>) => RollbackablePromise<T>;
+    rollback: <U>(rollbackFn: SyncRollbackFn<U> | AsyncRollbackFn<U>) => RollbackablePromise<T>;
 };
 
 export class LocalTransactionContext {
     static current: LocalTransactionContext | null = null;
     private rollbackStack: RollbackFn[] = [];
 
-    addRollback(fn: RollbackFn): void {
-        this.rollbackStack.push(fn);
+    addRollback<T>(fn: SyncRollbackFn<T>): void;
+    addRollback<T>(fn: AsyncRollbackFn<T>): void;
+    addRollback<T>(fn: SyncRollbackFn<T> | AsyncRollbackFn<T>): void {
+        const wrappedFn = async () => {
+            await Promise.resolve().then(fn);
+            return;
+        };
+        this.rollbackStack.push(wrappedFn);
     }
 
     async rollbackAll(): Promise<void> {
@@ -20,7 +28,7 @@ export class LocalTransactionContext {
             try {
                 await fn();
             } catch (error) {
-                throw new RollbackFailedError("rollback failed")
+                throw new RollbackFailedError("rollback failed");
             }
         }, Promise.resolve());
     }
