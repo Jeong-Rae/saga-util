c2eb9d2867be34c3ff46179764e3d27d562c84db
JRJR519 <kkwjdfo@gmail.com>
2025-03-20 06:47:15 +0900

    chore

--- index.ts
+++ index.ts
@@ -1,9 +1,6 @@
-import mongoose from 'mongoose';
 import LlmCallService from "./example/llmcall.service";
 import UserRepository from "./example/user.repository";
 import UsageRepository from "./example/usage.repository";
-
-mongoose.connect('mongodb+srv://home:8pGjOvDeG0CpR04i@cluster0.wqyssre.mongodb.net/test');
 
 
 const callService = new LlmCallService(
