b4fcaad9d843d6d6f6c043f66b1954a19fa01274
JRJR519 <kkwjdfo@gmail.com>
2025-05-29 01:10:28 +0900

    feat: 빌드 구성

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,6 +6,13 @@ export default defineConfig({
 	dts: true, // 타입 선언 파일(.d.ts) 생성
 	clean: true, // 빌드 전에 dist 폴더 비우기
 	outDir: "dist", // 출력 디렉토리
-	target: "es6",
+	target: "es2020",
 	minify: false, // 코드 압축 여부
+	splitting: false, // 코드 분할 비활성화
+	sourcemap: true, // 소스맵 생성
+	outExtension({ format }) {
+		return {
+			js: format === "cjs" ? ".js" : ".mjs",
+		};
+	},
 });
